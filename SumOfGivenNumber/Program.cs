8b4bc58d1d9d939ae630af549845fcd9780f0f12
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 22:19:56 +0500

    Add project files.

---
+++ SumOfGivenNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumOfGivenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Number");
+            int inputnumer =Int32.Parse(Console.ReadLine());
+            int backupvalue = inputnumer;
+            int RemainderNumber = 0, Sum = 0;
+
+            while (inputnumer != 0)
+            {
+
+                RemainderNumber = inputnumer % 10;
+                Sum = Sum + RemainderNumber;
+                inputnumer = inputnumer / 10;
+
+
+            }
+
+            Console.WriteLine("Sum of {0}  is {1}", backupvalue, Sum);
+
+        }
+    }
+}
